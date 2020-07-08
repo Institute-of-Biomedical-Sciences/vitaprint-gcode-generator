930d187e13b4f75227af7ccfa476b97698b84cad
bostjanvihar <bostjan@irnas.eu>
2020-07-08 16:09:26 +0200

    Add files via upload

---
+++ scaffoldGEN.py
@@ -0,0 +1,371 @@
+#
+#  Developed 2020 by Boštjan Vihar (bostjan AT irnas DOT eu)
+#
+
+'''
+
+This is a simple g-code generator for making wood-pile scaffold structures using Vitaprint and Planet CNC. Basic shape characteristics of the scaffold, as well as printing parameters can be specified.
+
+USAGE: 
+1. Copy scaffoldGEN.py to target directory and uncomment input/output commands
+2. Run script in terminal 
+
+'''
+
+# FURTHER DEVELOPMENT:
+# - rounded edges?
+
+import os
+import numpy as np
+
+
+'''
+### INPUT VALUES - uncomment to use as standalone
+side_l = float(input("Side length [mm], default = 10: ") or "10")
+bar_n = float(input("Number of bars [integer], default = 7: ") or "7")
+layer_n = float(input("Number of layers [integer], default = 5: ") or "5")
+layer_h = float(input("Layer height [mm], default = 0.2: ") or "0.2")
+feedrate = float(input("Printing speed [mm/min], default = 500: ") or "500")
+shape = input("Cross section shape square (s) or circle (c), default = s: ") or "s"
+drive = input("Extrusion drive pneumatic (p) or mechanical (m), default = p: ") or "p"
+if drive == "m":
+	dist_ext = float(input("Extrusion [mm] per 10mm distance, default = 0.1: ") or "0.1")
+	ramp = float(input("Ramp/retraction [mm], default = 0.05: ") or "0.05")
+	flow = input("Enable flow control [y/n], default = n: ") or "n"
+	if flow == 'y':
+		flow = '* #<_hw_jogpot>/511'
+	else:
+		flow = ''
+
+zsafe = float(input("Safe height after print [mm], default = 2: ") or "2")
+skirt_i = input("Add skirt [y/n], default = n: ") or "n"
+if skirt_i == "y":
+	sdist = float(input("Skirt distance from scaffold [mm], default = 2: ") or "2")
+else:
+	pass
+
+
+
+### GENERATE FILENAME
+if drive == "m":
+	fname = str(round(side_l))+'x'+str(round(side_l))+'mm_'+shape+'_bar'+str(int(bar_n))+'_h'+str(round(layer_h*10))+'e-1_a'+str(round(dist_ext*100))+'e-2'
+else:
+	fname = str(round(side_l))+'x'+str(round(side_l))+'mm_'+shape+'_bar'+str(int(bar_n))+'_h'+str(round(layer_h*10))+'e-1_pneum'
+'''
+
+
+#### CALCULATE COORDINATES ##################################################################
+def coordinates(side_l,bar_n,shape,layer_n):
+	
+	if bar_n % 2 == 0:
+		x,y = xy4(side_l,bar_n) # if number of bars is even, you require 4 steps to return to 0,0
+		if shape == 'cylinder': # if shape is a circle, recalculate x/y coordinates
+			x,y = circ4(side_l,x,y)
+		else:
+			pass
+		xn = 4
+
+	else:
+		x,y = xy2(side_l,bar_n) # if number of bars is odd, require 2 steps to return to 0,0
+		if shape == 'cylinder': # if shape is a circle, recalculate x/y coordinates
+			x,y = circ2(side_l,x,y)
+		else:
+			pass
+		xn = 2
+
+	r = (layer_n/xn) - int(layer_n/xn) # residual fraction of x/y array
+	# calculate all x coordinates
+	xt = np.tile(x,(int(layer_n/xn),1))
+	x = np.append(xt,x[:int(r*xn)], axis=0)
+	x = np.round(x,decimals=6)
+
+	# calculate all y coordinates
+	yt = np.tile(y,(int(layer_n/xn),1))
+	y = np.append(yt,y[:int(r*xn)], axis=0)
+	y = np.round(y,decimals=6)
+
+	#### CALCULATE DISTANCES BETWEEN POINTS
+	xflat=np.append([0],np.ravel(x),axis=0) #flatten x array and add a 0 at the beginning
+	yflat=np.append([0],np.ravel(y),axis=0)
+
+	n = np.size(xflat)
+	d = np.sqrt((xflat[0:n-1]-xflat[1:n])**2+(yflat[0:n-1]-yflat[1:n])**2) #calculate distances between all points
+
+	
+	
+
+	return x,y,n,d
+
+### SKIRT coordinates
+def skirt_c(sdist,side_l):
+	xskirt = np.array([-sdist, side_l+sdist, side_l+sdist, -sdist, -sdist,0])
+	yskirt = np.array([-sdist, -sdist, side_l+sdist, side_l+sdist, -sdist,0])
+	return xskirt, yskirt
+
+### ROTATIONS
+def xy2(side_l,bar_n): #2-STEP ROTATION
+
+	n = bar_n+1 #number of increments
+	d = side_l/n #distance between two bars
+
+	ni = np.repeat(np.arange(0,n+1,1),2) #make array with double indexes from 0 to n+1
+	nev = (ni%2==0).astype(int) #make a 1/0 array of even indexes
+
+## there...
+	x1=n*d*(nev)[:-1] #make array for x axis, crossing the structure every secong step: every even/odd value is 1/0, multiplied by length of structure
+	y1=d*(ni[1:]) #make array for y axis, growing incrementally
+
+## and back again
+	x2 = np.append(np.flip(y1)[1:],[0]) #y1 array in reversed order with app. finish point
+	y2 = np.append(np.flip(x1)[1:],[0]) #x1 array in reversed order with app. finish point
+## combine
+	x = np.array([x1,x2])
+	y = np.array([y1,y2])
+	return x,y
+
+def xy4(side_l,bar_n): #4-STEP ROTATION
+
+	n = bar_n+1 #number of increments
+	d = side_l/n #distance between two bars
+
+	ni = np.repeat(np.arange(0,n+1,1),2) #make array with double indexes from 0 to n+1
+	nev = (ni%2==0).astype(int) #make a 1/0 array of even indexes
+	
+## step 1
+	x1=n*d*(nev[:-1]) #make array for x axis, crossing the structure every secong step: every even/odd value is 1/0, multiplied by side_l
+	y1=d*(ni[1:]) #make array for y axis, growing incrementally
+
+## step 2
+	x2 = d*(ni[1:]) #
+	y2 = n*d*np.flip(nev[1:]) # 
+
+## step 3
+	x3 = np.flip(n*d*(nev[1:])) #
+	y3 = np.flip(d*(ni[:-1]))
+
+## step 4
+	x4 = np.flip(d*ni[:-1])# 
+	y4 = n*d*nev[:-1]#
+	
+## combine
+	x = np.array([x1,x2,x3,x4])
+	y = np.array([y1,y2,y3,y4])
+	return x,y
+
+
+### CYLINDRICAL SCAFFOLDS
+def circ2(side_l,x,y): #2-STEP ROTATION
+### new point calculations
+	d = side_l #simplify syntax
+
+	#for x
+	l = np.sqrt((d/2)**2-(d/2-y[0])**2) # calculate distances of x relative from vertical going through center
+	l[np.where(x[0]==0)]=-l[np.where(x[0]==0)] # determine where to add/subtract values from middle
+	x[0] = l + d/2 # subtract/add distances from middle
+
+	# for y
+	h = np.sqrt((d/2)**2-(d/2-x[1])**2)
+	h[np.where(y[1]==0)]=-h[np.where(y[1]==0)]
+	y[1] = h + d/2
+	
+	x = x[:,:-1] #remove last element of each row
+	y = y[:,:-1]
+	return x,y
+	
+def circ4(side_l,x,y): #4-STEP ROTATION
+	d = side_l
+
+	l = np.sqrt((d/2)**2-(d/2-y[0])**2)
+	l[np.where(x[0]==0)]=-l[np.where(x[0]==0)]
+	x[0] = l + d/2
+
+	h = np.sqrt((d/2)**2-(d/2-x[1])**2)
+	h[np.where(y[1]==0)]=-h[np.where(y[1]==0)]
+	y[1] = h + d/2
+
+	x[2] = h + d/2
+	y[3] = l + d/2
+	
+	x = x[:,:-1]
+	y = y[:,:-1]
+	return x,y
+
+
+#### WRITING TO FILE #####################################################################
+
+def pneumatic(fname,x,y,n,d,shape,side_l,layer_h,layer_n,skirt_i,sdist,feedrate,zsafe):
+	# starting g-code
+	# f = open(fname+'.gcode', "w")
+	longstring = '%\n' # first line of g-code
+	longstring += ('G92\tX0\tY0\n') #set xy coordinates to 0
+	longstring += ('G1\tZ0\tF{0}\n\n'.format(feedrate)) #go to Z0
+	# longstring += ('G1\tA[#<_a> +{0}] ;ramp\n\n'.format(ramp)) #ramp
+
+
+	# skirt
+	if skirt_i == 'on':
+	
+		xskirt,yskirt = skirt_c(sdist,side_l)
+	
+		longstring += ('M8\n') #open valve
+		for i in range(0,np.size(xskirt)):
+			skirt = 'G1\tX{0}\tY{1}\n'.format(xskirt[i],yskirt[i])
+			longstring += (skirt)
+		longstring += ('M9\n') #close valve
+		longstring += ('\n') #empty line
+	else:
+		pass
+
+	# main blocks	  # sample line G1 X4.8 Y1.5 A[#<_a> + 0.00038 * #<_hw_jogpot>/511]
+	h= np.shape(x)[0] # array height
+	w= np.shape(x)[1] # array width
+	g = 3 #first direction of rotation
+
+	if shape == 'cylinder': # if shape is a circle, draw circles
+		for j in range(0,h):
+			longstring += ('M8\n') #valve
+			for i in range(0,w):
+				if i % 2 == 0:
+					vrstica = 'G1\tX{0}\tY{1}\n'.format(x[j,i],y[j,i])
+				else:
+					vrstica = 'G{3}\tX{0}\tY{1}\tR{2}\n'.format(x[j,i],y[j,i],side_l/2,g)
+					if g == 3: # switch G2/3 direction
+						g = 2
+					else:
+						g = 3
+				longstring += (vrstica)
+			longstring += ('M9\n') #valve
+			zline = '\nG1\tZ[#<_z> +{0}]\n\n'.format(layer_h)
+			longstring += (zline)
+			if g == 3: # switch G2/3 direction
+				g = 2
+			else:
+				g = 3
+			if j<h-1:
+				jump = 'G{3}\tX{0}\tY{1}\tR{2}\tF1600\n'.format(x[j+1,0],y[j+1,0],side_l/2,g) #take FIRST xy elements of NEXT BLOCK
+				longstring += (jump)
+				longstring += ('G1\tF{0}\n'.format(feedrate))
+			else:
+				pass
+	else: # draw squares
+		for j in range(0,h):
+			longstring += ('M8\n') #valve
+			for i in range(0,w):
+				vrstica = 'G1\tX{0}\tY{1}\n'.format(x[j,i],y[j,i])
+				longstring += (vrstica)
+			longstring += ('M9\n') #valve
+			zline = '\nG1\tZ[#<_z> +{0}]\n\n'.format(layer_h)
+			longstring += (zline)
+
+	### end g-code
+	# longstring += ('G1\tA[#<_a> -{0}] ;retract\n'.format(ramp)) #retract
+	longstring += ('G1\tZ[#<_z> +{0}] ;go to safe height\n'.format(zsafe)) #go to safe height
+	longstring += ('G1\tX{0}\tY{1} ;go to next xy position\n'.format(side_l+5,0)) #go to next xy position
+	longstring += ('%')
+	
+	### write to file
+	# f = open(fname+'.gcode', "w")
+	# f.write(longstring)
+	# f.close()
+	return longstring
+
+	
+def piston(fname,x,y,n,d,shape,side_l,layer_h,layer_n,dist_ext,skirt_i,sdist,feedrate,zsafe,ramp,flow):
+
+	#### CALCULATE EXTRUSION "A" and reshape to x/y formats
+	aflat = d/10*dist_ext 
+	a = np.reshape(aflat,np.shape(x)) #reshape to x/y formats
+	a = np.round(a,decimals=6)
+	
+	# starting g-code
+	f = open(fname+'.gcode', "w")
+	longstring = '%\n' # first line of g-code
+	longstring += ('G92\tX0\tY0\n') #set xy coordinates to 0
+	longstring += ('G1\tZ0\tF{0}\n\n'.format(feedrate)) #go to Z0
+	longstring += ('G1\tA[#<_a> +{0}] ;ramp\n\n'.format(ramp)) #ramp
+	
+	if skirt_i == 'on':
+
+		### CALCULATE SKIRT
+		xskirt,yskirt = skirt_c(sdist,side_l)
+
+		askirt = np.full((1,4),(side_l+sdist)/10*dist_ext)
+		askirt = np.append([0],np.ravel(askirt),axis=0)
+		askirt = np.append(np.ravel(askirt),[0],axis=0)
+
+		# skirt
+		for i in range(0,np.size(xskirt)):
+			skirt = 'G1\tX{0}\tY{1}\tA[#<_a> +{2}]\n'.format(xskirt[i],yskirt[i],askirt[i])
+			longstring += (skirt)
+		longstring += ('\n') #empty line
+	else:
+		pass
+
+	# main blocks	  # sample line G1 X4.8 Y1.5 A[#<_a> + 0.00038 * #<_hw_jogpot>/511]
+	h= np.shape(x)[0] # array height
+	w= np.shape(x)[1] # array width
+	g = 3 #first direction of rotation
+
+	if shape == 'cylinder': # if shape is a circle, draw circles
+		for j in range(0,h):
+			for i in range(0,w):
+				if i % 2 == 0:
+					vrstica = 'G1\tX{0}\tY{1}\tA[#<_a> +{2}'.format(x[j,i],y[j,i],a[j,i])+flow+']\n'
+				else:
+					vrstica = 'G{4}\tX{0}\tY{1}\tR{3}\tA[#<_a> +{2}'.format(x[j,i],y[j,i],a[j,i],side_l/2,g)+flow+']\n'
+					if g == 3: # switch G2/3 direction
+						g = 2
+					else:
+						g = 3
+				longstring += (vrstica)
+			zline = '\nG1\tZ[#<_z> +{0}]\n\n'.format(layer_h)
+			longstring += (zline)
+			if g == 3: # switch G2/3 direction
+				g = 2
+			else:
+				g = 3
+			if j<h-1:
+				jump = 'G{3}\tX{0}\tY{1}\tR{2}\tF1600\n'.format(x[j+1,0],y[j+1,0],side_l/2,g) #take FIRST xy elements of NEXT BLOCK
+				longstring += (jump)
+				longstring += ('G1\tF{0}\n'.format(feedrate))
+			else:
+				pass
+	else: # draw squares
+		for j in range(0,h):
+			for i in range(0,w):
+				vrstica = 'G1\tX{0}\tY{1}\tA[#<_a> +{2}'.format(x[j,i],y[j,i],a[j,i])+flow+']\n'
+				longstring += (vrstica)
+			zline = '\nG1\tZ[#<_z> +{0}]\n\n'.format(layer_h)
+			longstring += (zline)
+
+	### end g-code
+	longstring += ('G1\tA[#<_a> -{0}] ;retract\n'.format(ramp)) #retract
+	longstring += ('G1\tZ[#<_z> +{0}] ;go to safe height\n'.format(zsafe)) #go to safe height
+	longstring += ('G1\tX{0}\tY{1} ;go to next xy position\n'.format(side_l+5,0)) #go to next xy position
+	longstring += ('%')
+	
+	### write to file
+	# f = open(fname+'.gcode', "w")
+	# f.write(longstring)
+	# f.close()
+	return longstring
+	
+
+
+###### EXECUTE G-CODE GENERATOR
+'''
+x,y,n,d = coordinates()
+
+if skirt_i == "y":
+	xskirt,yskirt = skirt_c()
+else:
+	pass
+
+if drive == "m":
+	piston()
+else:
+	pneumatic()
+'''
+# seq = open(fname+'.gcode', "r")
+# text = seq.read()
+# print(text)
---
+++ scaffoldGUI.py
@@ -0,0 +1,399 @@
+#
+#  Developed 2020 by Boštjan Vihar (bostjan AT irnas DOT eu) and Luka Banović (banovic AT irnas DOT eu)
+#
+
+'''
+
+This is a simple graphical user interface for the Vitaprint g-code generator for making wood-pile scaffold structures using Vitaprint and Planet CNC. Basic shape characteristics of the scaffold, as well as printing parameters can be specified.
+
+USAGE: 
+a) Within Planet CNC (ask for additional info)
+b) Run script in terminal (copy scaffoldGUI.py and scaffoldGEN.py to same target directory)
+
+'''
+
+import sys
+import os
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel, QLineEdit, QPushButton, QComboBox, QCheckBox, QFileDialog
+from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QIcon, QPixmap
+
+import numpy as np
+from scaffoldGEN import *
+
+### CHECK IF SCRIPT IS RUNNING IN PLANET CNC
+planetcnc_enabled = False
+try:
+	import planetcnc
+	import gcode
+	planetcnc_enabled = True
+	print("Running under PlanetCNC - OK")
+except:
+	print("Not running under Planet CNC")
+
+
+### GUI
+class MainWindow(QMainWindow):
+	def __init__(self):
+		QMainWindow.__init__(self)
+
+		self.setMinimumSize(QSize(800, 380))	
+		self.setWindowTitle("Vitaprint scaffold generator") 
+
+		
+		self.img = QLabel(self) # schematic
+		self.img.setPixmap(QPixmap("schematic.png"))
+		self.img.setScaledContents(True)
+		self.img.move(280, 45)
+		self.img.resize(500, 240)
+
+
+		### SCAFFOLD SHAPE
+		self.shape = QLabel(self) # Select scaffold shape
+		self.shape.setText('Scaffold shape:')
+		self.shape.move(20,20)
+		self.shape.resize(135,20)
+		
+		self.shape_state = QLabel(self) # Current scaffold shape STATE
+		self.shape_state.setText('cuboid') # default value
+		self.shape_state.move(160,18)
+		
+		self.shape_i = QComboBox(self) # Drop-down menu scaffold shape
+		self.shape_i.addItem('cuboid')
+		self.shape_i.addItem('cylinder')
+		self.shape_i.move(160,20)
+		self.shape_i.resize(100, 20)
+		
+		self.shape_i.activated[str].connect(self.shape_choice) # call function which returns the selected shape
+		
+		### EXTRUSION DRIVE
+		self.extrusion = QLabel(self) # Select extrusion drive
+		self.extrusion.setText('Extrusion mechanism:')
+		self.extrusion.move(20, 50)
+		self.extrusion.resize(135, 20)
+		
+		self.extrusion_state = QLabel(self) # Current scaffold shape STATE
+		self.extrusion_state.setText('mechanical') # default value
+		self.extrusion_state.move(160,48)
+		
+		self.extrusion_i = QComboBox(self)
+		self.extrusion_i.addItem('mechanical')
+		self.extrusion_i.addItem('pneumatic')
+		self.extrusion_i.move(160,50)
+		self.extrusion_i.resize(100, 20)
+		
+		self.extrusion_i.activated[str].connect(self.extr_choice) # call function which returns the selected extrusion drive
+
+		self.E = QLabel(self) # Extrusion rate
+		self.E.setText('Extrusion rate [mm/10mm]:') #only works if mechanical
+		self.E.move(20, 73)
+		self.E.resize(135, 20)
+		
+		self.E_i = QLineEdit(self)
+		self.E_i.setText('0.2')
+		self.E_i.move(160, 73)
+		self.E_i.resize(100, 20)
+		
+		self.retramp = QLabel(self) # Extrusion rate
+		self.retramp.setText('Ramp/retraction [mm]:') #only works if mechanical
+		self.retramp.move(20, 95)
+		self.retramp.resize(135, 20)
+		
+		self.ramp_i = QLineEdit(self)
+		self.ramp_i.setText('0.05')
+		self.ramp_i.move(160, 95)
+		self.ramp_i.resize(100, 20)
+				
+		self.flowc = QLabel(self) # Flow control
+		self.flowc.setText('Flow control:') #only works if mechanical
+		self.flowc.move(20, 110)
+		self.flow_s = 'off'
+		
+		self.flowc_i = QCheckBox(self)
+		self.flowc_i.stateChanged.connect(self.flowstate)
+		self.flowc_i.move(160,110)
+
+
+		### SIZE
+		self.diameter = QLabel(self)
+		self.diameter.setText('Side length/diameter [mm]:')
+		self.diameter.move(20, 140)
+		self.diameter.resize(135, 20)
+		
+		self.diameter_i = QLineEdit(self)
+		self.diameter_i.setText('10')
+		self.diameter_i.move(160, 140)
+		self.diameter_i.resize(100, 20)
+		
+		
+		### N OF BARS
+		self.nbar = QLabel(self)
+		self.nbar.setText('Number of bars:')
+		self.nbar.move(20, 165)
+		
+		self.nbar_i = QLineEdit(self)
+		self.nbar_i.setText('7')
+		self.nbar_i.move(160, 170)
+		self.nbar_i.resize(100, 20)
+				
+		
+		### N-LAYERS
+		self.nlay = QLabel(self)
+		self.nlay.setText('Number of layers:')
+		self.nlay.move(20, 195)
+		
+		self.nlay_i = QLineEdit(self)
+		self.nlay_i.setText('5')
+		self.nlay_i.move(160, 200)
+		self.nlay_i.resize(100, 20)
+		
+		
+		### LAYER HEIGHT
+		self.lh = QLabel(self)
+		self.lh.setText('Layer height [mm]:')
+		self.lh.move(20, 225)
+		
+		self.lh_i = QLineEdit(self)
+		self.lh_i.setText('0.2')
+		self.lh_i.move(160, 230)
+		self.lh_i.resize(100, 20)
+		
+		
+		### FEEDRATE
+		self.feed = QLabel(self)
+		self.feed.setText('Feedrate [mm/min]:')
+		self.feed.move(20, 255)
+		
+		self.feed_i = QLineEdit(self)
+		self.feed_i.setText('500')
+		self.feed_i.move(160, 260)
+		self.feed_i.resize(100, 20)
+		
+
+		### SKIRT
+		self.skirt = QLabel(self) # Flow control
+		self.skirt.setText('Skirt:') #only works if mechanical
+		self.skirt.move(20, 285)
+		self.skirt_s = 'on'
+		
+		self.skirt_i = QCheckBox(self)
+		self.skirt_i.setChecked(True)
+		self.skirt_i.stateChanged.connect(self.skirtyn)
+		self.skirt_i.move(160,285)
+		
+		self.sdist = QLabel(self) # Flow control
+		self.sdist.setText('Skirt distance [mm]:') #only works if mechanical
+		self.sdist.move(20, 305)
+		# self.sdist_s = 'on'
+		
+		self.sdist_i = QLineEdit(self)
+		self.sdist_i.setText('2')
+		self.sdist_i.move(160,310)		
+		self.sdist_i.resize(100, 20)
+		
+
+		### GENERATE BUTTON
+		genbut = QPushButton('Generate!', self)
+		genbut.setStyleSheet("background-color: green; color: white")
+		genbut.clicked.connect(self.clickMethod)
+		genbut.resize(100, 20)
+		genbut.move(400,300)
+		
+		clbut = QPushButton('close', self)
+		clbut.clicked.connect(self.close_app)
+		clbut.resize(100,20)
+		clbut.move(510,300)
+		
+		
+		### FILE SAVING
+		self.save_to_file = QLabel(self) # Select scaffold shape
+		self.save_to_file.setText('Save to file:')
+		self.save_to_file.move(20,340)
+		self.save_to_file.resize(135,20)
+		self.save_s = 'off'
+		
+		self.save_i = QCheckBox(self)
+		self.save_i.setChecked(False)
+		self.save_i.stateChanged.connect(self.saveyn)
+		self.save_i.move(160,336)
+		
+		self.browse = QPushButton('Choose Directory', self)
+		self.browse.clicked.connect(self.browse_dir)
+		self.browse.resize(100, 20)
+		self.browse.move(180,340)
+		self.browse_i = 'local'
+		self.browse.hide()
+		
+		
+	### GENERATING THE SCAFFOLD!!!! #######################################################
+	def clickMethod(self,state):
+		print('Scaffold shape: ' + self.shape_state.text())
+		print('Extrusion mechanism: ' + self.extrusion_state.text())
+		if self.extrusion_state.text() == 'mechanical':
+			print('Extrusion rate: ' + self.E_i.text() + 'mm/10mm of path')
+			print('Flow control: ' + self.flow_s)
+		else:
+			pass
+		print('Side length/diameter: ' + self.diameter_i.text() + 'mm')
+		print('Number of bars: ' + self.nbar_i.text())
+		print('Number of layers: ' + self.nlay_i.text())
+		print('Layer height: ' + self.lh_i.text() + 'mm')
+		print('Feedrate: ' + self.feed_i.text() + 'mm/min')
+		print('Skirt: '+ self.skirt_s)
+		if self.skirt_s == 'on':
+			print('Skirt distance: '+ self.sdist_i.text() + 'mm')
+		else:
+			pass
+		print('Save to file: '+ self.save_s)
+		# if self.save_i == 'on':
+			# print('File path: '+ save_directory.text() )
+		# else:
+			# pass
+		# print(self.browse_i)
+		
+		#### PASS INPUT TO VARIABLES
+		side_l = float(str(self.diameter_i.text()))
+		bar_n = float(self.nbar_i.text())
+		layer_n = float(self.nlay_i.text())
+		layer_h = float(self.lh_i.text())
+		feedrate = float(self.feed_i.text())
+		shape = self.shape_state.text()
+		drive = self.extrusion_state.text()
+		if drive == "mechanical":
+			dist_ext = float(self.E_i.text())
+			ramp = float(self.ramp_i.text()) #0.05 ######### tu še input!
+			flow = self.flow_s
+			if flow == 'on':
+				flow = '* #<_hw_jogpot>/511'
+			else:
+				flow = ''
+
+		zsafe = 2 
+		skirt_in = self.skirt_s
+		if skirt_in == "on":
+			sdist_in = float(self.sdist_i.text())
+		else:
+			sdist_in = "off"
+			
+		saving = self.save_s # state of file saving
+			
+
+
+		### GENERATE FILENAME
+		if drive == "mechanical":
+			fname = str(round(side_l))+'x'+str(round(side_l))+'mm_'+shape+'_bar'+str(int(bar_n))+'_h'+str(round(layer_h*10))+'e-1_a'+str(round(dist_ext*100))+'e-2'
+		else:
+			fname = str(round(side_l))+'x'+str(round(side_l))+'mm_'+shape+'_bar'+str(int(bar_n))+'_h'+str(round(layer_h*10))+'e-1_pneum'
+		
+		
+		### EXECUTE G-CODE GENERATOR
+		x,y,n,d = coordinates(side_l,bar_n,shape,layer_n)
+
+		if drive == "mechanical":
+			longstring = piston(fname,x,y,n,d,shape,side_l,layer_h,layer_n,dist_ext,skirt_in,sdist_in,feedrate,zsafe,ramp,flow)
+		else:
+			longstring = pneumatic(fname,x,y,n,d,shape,side_l,layer_h,layer_n,skirt_in,sdist_in,feedrate,zsafe)
+		
+		### FILE SAVER
+		if saving == "on":
+			if self.browse_i == 'local':
+				pass
+			else:
+				os.chdir(self.browse_i)
+			
+			f = open(fname+'.gcode', "w")
+			f.write(longstring)
+			f.close()
+		else:
+			pass
+		
+		### ADD G-CODE TO PLANET
+		if planetcnc_enabled:
+			lines = longstring.splitlines()
+			for line in lines:
+				gcode.lineAdd(line)
+			
+			self.close()
+
+		
+	#######################################################################################
+	
+	def close_app(self):
+		print('G-code generator closed')
+		self.close() # je za to kaka boljša komanda?
+		# self.quit()
+		# self.process.kill()
+		# self.commTerminate()
+		
+		# sys.exit() # to zapre cel program!
+		
+	def shape_choice(self,text):
+		self.shape_state.setText(text)
+		
+	def extr_choice(self,text):
+		# try:
+		self.extrusion_state.setText(text)
+		if self.extrusion_state.text() == 'pneumatic':
+			self.E.hide()
+			self.E_i.hide()
+			self.flowc.hide()
+			self.flowc_i.hide()
+			self.retramp.hide()
+			self.ramp_i.hide()
+		else:
+			self.E.show()
+			self.E_i.show()
+			self.flowc.show()
+			self.flowc_i.show()
+			self.retramp.show()
+			self.ramp_i.show()
+			
+			self.extrusion_i.setFocus()
+		# except:
+			# print('warning - drive change unsuccessful')
+		
+	def flowstate(self,state):
+		if state == QtCore.Qt.Checked:
+			flow_s = 'on'
+		else:
+			flow_s = 'off'
+
+		self.flow_s = flow_s	
+		
+	def skirtyn(self,state):
+		if state == QtCore.Qt.Checked:
+			skirt_s = 'on'
+			self.sdist.show()
+			self.sdist_i.show()
+		else:
+			skirt_s = 'off'
+			self.sdist.hide()
+			self.sdist_i.hide()
+
+		self.skirt_s = skirt_s
+
+	def saveyn(self,state):
+		if state == QtCore.Qt.Checked:
+			save_s = 'on'
+			self.browse.show()
+		else:
+			save_s = 'off'
+			self.browse.hide()
+			
+		self.save_s = save_s
+			
+	def browse_dir(self):
+		browse_i = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+		self.browse_i = browse_i
+
+
+if __name__ == "__main__":
+	app = QtWidgets.QApplication(sys.argv)
+	mainWin = MainWindow()
+	mainWin.show()
+	app.exec_()
+	# app.closeAllWindows()
+	app.quit()
+	# sys.exit(app.exec_())
+	# sys.exit() # to zapre cel program!
